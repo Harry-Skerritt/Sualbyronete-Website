d90ca4ca42226f3da1f77d01bd1778346b7b34a1
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-06-03 14:46:39 +0100

    Added a variable for mobile size and added mobile styling for the puppy card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import path from 'path';
 
 // GitHub stuff
 const isProduction = import.meta.env.PROD;
@@ -13,5 +14,15 @@ export default defineConfig({
 
     // GitHub Stuff - Remove before final build
     site: 'https://harry-skerritt.github.io',
-    base: isProduction ? '/Sualbyronete-Website' : '/'
+    base: isProduction ? '/Sualbyronete-Website' : '/',
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "${path.resolve('./src/styles/variables')}" as *;`
+                }
+            }
+        }
+    }
 });
