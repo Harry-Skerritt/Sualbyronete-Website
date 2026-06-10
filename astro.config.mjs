5e01ec3b138952e5a77d80d52575328185ac732e
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-06-10 22:39:42 +0100

    Added robots.txt and sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,31 @@ import { defineConfig } from 'astro/config';
 import path from 'path';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    trailingSlash: 'never',
-    output: 'server',
-    adapter: cloudflare({
-        imageService: 'passthrough',
-        configPath: './wrangler.jsonc',
-        remoteBindings: true,
-    }),
-    base: '',
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `@use "${path.resolve('./src/styles/variables')}" as *;`
-                }
-            }
-        },
-    },
+  site: 'https://sualbyronete.co.uk',
+  trailingSlash: 'never',
+  output: 'server',
+
+  adapter: cloudflare({
+      imageService: 'passthrough',
+      configPath: './wrangler.jsonc',
+      remoteBindings: true,
+  }),
+
+  base: '',
+
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: `@use "${path.resolve('./src/styles/variables')}" as *;`
+              }
+          }
+      },
+  },
+
+  integrations: [sitemap()],
 });
